b6af8bd523fd3bbfda18ea1543bbae2508bd8d61
Chris <81205126+TalentedB@users.noreply.github.com>
2023-05-16 20:58:44 -0700

    Fixed Threshold not being changable

--- main.py
+++ main.py
@@ -36,7 +36,10 @@ def compare_frames(frame1, frame2):
 	return percentage_changed
 
 
-def ExtractSlidesToPDF(video_path: str = 'thevideo.mp4', output_path: str = 'slides.pdf', threshold: int = 3):
+def ExtractSlidesToPDF(video_path: str = 'thevideo.mp4', output_path: str = 'slides.pdf', threshold: int = 2):
+	
+	if threshold == "":
+		threshold = 2
 
 	# Create a VideoCapture object and read from input file
 	print("Extracting Slides...")
@@ -51,7 +54,8 @@ def ExtractSlidesToPDF(video_path: str = 'thevideo.mp4', output_path: str = 'sli
 	# Get first frame
 	ret, frame = cap.read()
 	prev_frame = frame
-
+	cv2.imwrite(f'temp/temp{len(image_list)}.png', cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE))
+	image_list.append(f'temp/temp{len(image_list)}.png')
 
 
 	# Read until video is completed
@@ -62,16 +66,15 @@ def ExtractSlidesToPDF(video_path: str = 'thevideo.mp4', output_path: str = 'sli
 		# Capture frame-by-frame
 		ret, frame = cap.read()
 		if ret == True:
-			if compare_frames(frame, prev_frame) > 2:
+			if compare_frames(frame, prev_frame) > threshold:
 				cv2.imwrite(f'temp/temp{len(image_list)}.png', cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE))
 				image_list.append(f'temp/temp{len(image_list)}.png')
-			prev_frame = frame
-				
-	# Break the loop
+			prev_frame = frame		
+	# Break the loop if done reading
 		else:
 			break
 
-
+	# Combine all images into a PDF
 	for image in image_list:
 		pdf.add_page()
 		pdf.image(image, 0, 0, 210, 297)
@@ -82,7 +85,6 @@ def ExtractSlidesToPDF(video_path: str = 'thevideo.mp4', output_path: str = 'sli
 	cap.release()
 
 	# Closes all the frames
-	cap.release()
 	cv2.destroyAllWindows()
 
 def initProgram():
@@ -94,13 +96,13 @@ def initProgram():
 	print("3. Exit")
 	choice = input("Enter your choice: ")
 	if choice == "1":
-		threshold = input("Enter the threshold (default is 3): ")
+		threshold = input("Enter the threshold (default is 2): ")
 		video_path = input("Enter the video path: ")
 		ExtractSlidesToPDF(threshold=threshold, video_path=video_path)
 		print("Exported Slides Successfully")
 		exit(0)
 	if choice == "2":
-		threshold = input("Enter the threshold (default is 3): ")
+		threshold = input("Enter the threshold (default is 2): ")
 		video_path = input("Enter the video link (youtube): ")
 		Download(link=video_path)
 		ExtractSlidesToPDF(threshold=threshold, video_path="temp/tempvideo.mp4")
@@ -117,10 +119,6 @@ def initProgram():
 
 
 initProgram()
-
-
-# ExtractSlidesToPDF(threshold=3, video_path="Chapter 1.mp4")
-
 
 
 
